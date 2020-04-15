a26dd6a2516db278568d934abfb246f407ebb135
Larry Botha <larry@fixate.it>
2020-04-14 20:40:45 -0500

    add note

--- src/03-break-down-requirement-into-small-problems/index.ts
+++ src/03-break-down-requirement-into-small-problems/index.ts
@@ -1,5 +1,8 @@
 import { Observable } from "rxjs";
 
+/**
+ * Create observables for each side effect
+ */
 const taskStarts = new Observable();
 const taskCompletions = new Observable();
 const showLoader = new Observable();
