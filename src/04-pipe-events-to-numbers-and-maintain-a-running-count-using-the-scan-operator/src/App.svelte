ec868c16ecba5e38979987f3f3dbb156c9ae0f2b
Larry Botha <larry@fixate.it>
2020-04-15 22:48:59 -0500

    add example

---
+++ src/04-pipe-events-to-numbers-and-maintain-a-running-count-using-the-scan-operator/src/App.svelte
@@ -0,0 +1,29 @@
+<script type="typescript">
+  import { activeTasksCount$ } from "./streams/index.ts";
+
+  export let name: string;
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<main>{$activeTasksCount$}</main>
---
+++ src/04-pipe-events-to-numbers-and-maintain-a-running-count-using-the-scan-operator/src/streams/index.ts
@@ -0,0 +1,32 @@
+import { Observable, merge } from "rxjs";
+import { mapTo, scan } from "rxjs/operators";
+
+const taskStart$ = new Observable();
+const taskCompleted$ = new Observable();
+const showLoader$ = new Observable();
+
+/**
+ * map each event on taskStart$ to 1, and -1 when a task completes
+ */
+const taskIncrement$ = taskStart$.pipe(mapTo(1));
+const taskDecrement$ = taskCompleted$.pipe(mapTo(-1));
+
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXxxxXXXX
+
+/**
+ * We create a new observable by merging the two streams
+ *
+ * This observable will emit all events from all streams as they happen
+ */
+const taskIncDec$ = merge(taskIncrement$, taskDecrement$);
+
+/**
+ * We take the task increment / decrement stream, and reduce it.
+ *
+ * scan behaves in the same way as [].reduce
+ */
+const activeTasksCount$ = taskIncDec$.pipe(
+  scan((acc, incDec) => acc + incDec, 0)
+);
+
+export { activeTasksCount$ };
