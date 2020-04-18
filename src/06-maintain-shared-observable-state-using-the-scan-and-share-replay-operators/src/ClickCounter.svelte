9d01e6c62234e37f876dd14b1d5bfd27ff0e3352
Larry Botha <larry@fixate.it>
2020-04-18 17:20:12 -0500

    render subscription values

--- src/06-maintain-shared-observable-state-using-the-scan-and-share-replay-operators/src/ClickCounter.svelte
+++ src/06-maintain-shared-observable-state-using-the-scan-and-share-replay-operators/src/ClickCounter.svelte
@@ -2,15 +2,33 @@
   import { onDestroy, onMount } from "svelte";
 
   export let clickCountFactory;
+  export let name;
   let buttonEl1;
   let clickCount$;
   let subscribers = [];
+  let outputs = [];
+
+  function createSub(output) {
+    return function handleEvent(value) {
+      output = { ...output, value };
+      outputs = outputs.map(o => {
+        if (o.id === output.id) {
+          return output;
+        }
+
+        return o;
+      });
+
+      console.log(`${name} subscriber: ${output.id} =>`, value);
+    };
+  }
 
   function addSubscriber() {
     const length = subscribers.length;
-    const subscription = clickCount$.subscribe(ev =>
-      console.log(`subscriber: ${length} =>`, ev)
-    );
+    let output = { id: subscribers.length };
+    outputs = outputs.concat(output);
+
+    const subscription = clickCount$.subscribe(createSub(output));
 
     subscribers = subscribers.concat(subscription);
   }
@@ -18,6 +36,7 @@
   function removeAllSubscribers() {
     subscribers.map(sub => sub.unsubscribe());
 
+    outputs = [];
     subscribers = [];
   }
 
@@ -32,10 +51,39 @@
   });
 </script>
 
-{#each subscribers as sub}{/each}
+<style>
+  table {
+    border-collapse: collapse;
+  }
 
-<button bind:this={buttonEl1}>increment count</button>
+  thead {
+    font-weight: bold;
+  }
+  table td {
+    border: 1px solid;
+  }
+</style>
 
-<button on:click={addSubscriber}>add subscriber</button>
+<p>
+  <button bind:this={buttonEl1}>increment count</button>
 
-<button on:click={removeAllSubscribers}>remove subscribers</button>
+  <button on:click={addSubscriber}>add subscriber</button>
+
+  <button on:click={removeAllSubscribers}>remove subscribers</button>
+</p>
+
+<table>
+  <thead>
+    <td>subscriber</td>
+    <td>click count</td>
+  </thead>
+
+  <tbody>
+    {#each outputs as { id, value } (id)}
+      <tr>
+        <td>{id}</td>
+        <td>{value}</td>
+      </tr>
+    {/each}
+  </tbody>
+</table>
